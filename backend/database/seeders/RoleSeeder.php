6f0acdb56e07e4fe657ec7facd8cc747bb946fe1
dawid <dawidmetelski00@gmail.com>
2025-11-15 12:36:31 +0100

    create roles

---
+++ backend/database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'admin'],
+            ['name' => 'agent'],
+            ['name' => 'reporter'],
+        ];
+
+        foreach($roles as $role) {
+            Role::firstOrCreate($role);
+        }
+    }
+}
